b727cf003d95be17960f6ba85b991861684b2e27
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-01-10 11:14:27 -0500

    Import datetime in tasks

--- norrin/notifications/tasks.py
+++ norrin/notifications/tasks.py
@@ -1,3 +1,4 @@
+import datetime
 import urbanairship as ua
 from celery import Celery
 from celery.utils.log import get_task_logger
--- runtasks.sh
+++ runtasks.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-celery worker -A norrin.notifications.tasks -l INFO -f celerybeat.stdout.log --beat
+# celery worker -A norrin.notifications.tasks -l INFO -f celerybeat.stdout.log --beat
+
+python -c "from norrin.notifications import tasks; tasks.run_bill_service()"
+python -c "from norrin.notifications import tasks; tasks.run_bill_action_service()"
+python -c "from norrin.notifications import tasks; tasks.run_upcoming_bill_service()"
+python -c "from norrin.notifications import tasks; tasks.run_vote_service()"
