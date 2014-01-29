42717f31b71dec9f3a9ed3aa4588ea8d05011a06
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-01-29 10:32:28 -0500

    trivial change bc heroku

--- norrin/settings.py
+++ norrin/settings.py
@@ -85,6 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
+# Man this is stuff.
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
