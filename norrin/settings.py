446bc64d2dc462ab7be969f44193258d42881417
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-01-29 10:15:49 -0500

    This?

--- norrin/settings.py
+++ norrin/settings.py
@@ -85,7 +85,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 # STATICFILES_DIRS = (
