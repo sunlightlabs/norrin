d0d725af21db28e6ae846d4eb5d931d8c1650511
Daniel Cloud <dcloud@sunlightfoundation.com>
2014-01-28 19:45:46 -0500

    Lets not search for static assets in a directory that does not exist (and thus doesn't have statc assets)

--- norrin/settings.py
+++ norrin/settings.py
@@ -88,9 +88,9 @@ USE_TZ = True
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # other stuff
 
