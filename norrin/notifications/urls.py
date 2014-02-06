33987b54cdc8e4c5228d0b8fc01ecc616eb002c3
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-02-06 14:27:53 -0500

    view all notifications

--- norrin/notifications/urls.py
+++ norrin/notifications/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from norrin.notifications.views import StatusView, NotificationView
+from norrin.notifications.views import StatusView, NotificationView, NotificationListView
 
 urlpatterns = patterns('',
     url(r'^status/$', StatusView.as_view()),
+    url(r'^all/$', NotificationListView.as_view()),
     url(r'^(?P<notification_id>\w{32})/$', NotificationView.as_view(), name='notification_detail'),
 )
--- norrin/notifications/views.py
+++ norrin/notifications/views.py
@@ -1,3 +1,5 @@
+import math
+
 from django.shortcuts import render
 from django.views.generic import View
 from mongokit import Connection
@@ -16,7 +18,7 @@ class StatusView(View):
 
     def get(self, request, *args, **kwargs):
         context = {
-            'recent_notifications': [n for n in db.notifications.find({}).limit(20).sort("timestamp", -1)],
+            'recent_notifications': [n for n in db.notifications.find({}).limit(5).sort("timestamp", -1)],
         }
         return render(request, 'notifications/status.html', context)
 
@@ -28,3 +30,31 @@ class NotificationView(View):
             'notification': db.notifications.find_one({'id': kwargs.get('notification_id')})
         }
         return render(request, 'notifications/notification.html', context)
+
+
+class NotificationListView(View):
+
+    per_page = 20
+
+    def get(self, request, *args, **kwargs):
+
+        page = int(request.GET.get('page') or 1)
+        offset = (page - 1) * self.per_page
+
+        qs = db.notifications.find({})
+        notifications = qs.sort('timestamp', -1).limit(self.per_page).skip(offset)
+
+        total_count = qs.count()
+        total_pages = int(math.ceil(total_count / float(self.per_page)))
+
+        context = {
+            'notifications': [n for n in qs],
+            'pages': {
+                'current': page,
+                'total': total_pages,
+                'next_page': page + 1 if page < total_pages else None,
+                'previous_page': page - 1 if page > 1 else None,
+            }
+        }
+
+        return render(request, 'notifications/notification_list.html', context)
