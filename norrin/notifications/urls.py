61860b7be5d643361f3d5aed0b3524623a5e811b
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-02-10 17:57:24 -0500

    make notification urls nicer

--- norrin/notifications/urls.py
+++ norrin/notifications/urls.py
@@ -4,6 +4,6 @@ from django.contrib.auth.decorators import login_required
 from norrin.notifications.views import NotificationView, NotificationListView
 
 urlpatterns = patterns('',
-    url(r'^all/$', login_required(NotificationListView.as_view()), name='notification_list'),
     url(r'^(?P<notification_id>\w{8,32})/$', login_required(NotificationView.as_view()), name='notification_detail'),
+    url(r'^$', login_required(NotificationListView.as_view()), name='notification_list'),
 )
