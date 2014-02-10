a96b6873056a91172957779c2ce573e8a5290631
Jeremy Carbaugh <jcarbaugh@gmail.com>
2014-02-10 18:06:55 -0500

    refactor templates and add blank app config view

--- norrin/appconfig/urls.py
+++ norrin/appconfig/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 from norrin.appconfig.views import ConfigurationView
 
 urlpatterns = patterns('',
     url(r'^(?P<key>\w{12})(?P<client_key>\w{12})(?:\.(?P<format>plist|json))?$', ConfigurationView.as_view()),
     url(r'^(?P<key>\w{12})(?:\.(?P<format>plist|json))?$', ConfigurationView.as_view(), {'client_key': None}),
+    url(r'^$', TemplateView.as_view(template_name='appconfig/index.html')),
 )
